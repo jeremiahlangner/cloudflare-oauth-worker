5f851eecaeb67102f8ddadacfac046ae9fa28948
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-09-08 12:30:37 -0700

    work in first build

--- build/worker.js
+++ build/worker.js
@@ -1,21 +1,41 @@
 "use strict";
 (() => {
+  // node_modules/simple-worker-router/build/router.js
+  var o = class {
+    routes;
+    constructor(t = []) {
+      this.routes = [];
+      for (let e of t)
+        this.register(...e);
+    }
+    register(t, e, n = "GET") {
+      this.routes.push({ path: new URLPattern({ pathname: t }), method: n, handler: e });
+    }
+    handle(t) {
+      let { request: e } = t;
+      for (let n of this.routes) {
+        if (n.method !== e.method)
+          continue;
+        if (n.path.exec({ pathname: new URL(e.url).pathname }))
+          return n.handler(t);
+      }
+      return new Response("Not found", { status: 404 });
+    }
+  };
+
   // src/worker.ts
-  function handler(params) {
-  }
   function authorize() {
   }
   var worker_default = {
     async fetch(request, env, ctx) {
-      const url = new URL(request.url);
-      const router = new (void 0)([
+      const router = new o([
         ["/authorize", authorize],
         ["/code", authorize],
         ["/token", authorize],
         ["/login", authorize],
         ["/resource", authorize]
       ]);
-      return handler({ request, env, ctx });
+      return router.handle({ request, env, ctx });
     }
   };
 })();
