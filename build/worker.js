64cb2ddc0fc24754296bbf12c46994e26ccda566
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-09-08 12:33:06 -0700

    build as esm

--- build/worker.js
+++ build/worker.js
@@ -1,41 +1,41 @@
-"use strict";
-(() => {
-  // node_modules/simple-worker-router/build/router.js
-  var o = class {
-    routes;
-    constructor(t = []) {
-      this.routes = [];
-      for (let e of t)
-        this.register(...e);
-    }
-    register(t, e, n = "GET") {
-      this.routes.push({ path: new URLPattern({ pathname: t }), method: n, handler: e });
-    }
-    handle(t) {
-      let { request: e } = t;
-      for (let n of this.routes) {
-        if (n.method !== e.method)
-          continue;
-        if (n.path.exec({ pathname: new URL(e.url).pathname }))
-          return n.handler(t);
-      }
-      return new Response("Not found", { status: 404 });
-    }
-  };
-
-  // src/worker.ts
-  function authorize() {
+// node_modules/simple-worker-router/build/router.js
+var o = class {
+  routes;
+  constructor(t = []) {
+    this.routes = [];
+    for (let e of t)
+      this.register(...e);
   }
-  var worker_default = {
-    async fetch(request, env, ctx) {
-      const router = new o([
-        ["/authorize", authorize],
-        ["/code", authorize],
-        ["/token", authorize],
-        ["/login", authorize],
-        ["/resource", authorize]
-      ]);
-      return router.handle({ request, env, ctx });
+  register(t, e, n = "GET") {
+    this.routes.push({ path: new URLPattern({ pathname: t }), method: n, handler: e });
+  }
+  handle(t) {
+    let { request: e } = t;
+    for (let n of this.routes) {
+      if (n.method !== e.method)
+        continue;
+      if (n.path.exec({ pathname: new URL(e.url).pathname }))
+        return n.handler(t);
     }
-  };
-})();
+    return new Response("Not found", { status: 404 });
+  }
+};
+
+// src/worker.ts
+function authorize() {
+}
+var worker_default = {
+  async fetch(request, env, ctx) {
+    const router = new o([
+      ["/authorize", authorize],
+      ["/code", authorize],
+      ["/token", authorize],
+      ["/login", authorize],
+      ["/resource", authorize]
+    ]);
+    return router.handle({ request, env, ctx });
+  }
+};
+export {
+  worker_default as default
+};
