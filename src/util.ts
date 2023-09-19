5448d1ba57d1a89d9535b5f322b4117d7ff77bc6
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-09-19 08:05:33 -0700

    add JSONResponse type

---
+++ src/util.ts
@@ -0,0 +1,30 @@
+function html(strings: TemplateStringsArray, ...values: any[]): string {
+  return String.raw({ raw: strings }, ...values);
+}
+
+function hash(_hashes: { [key: string]: string }): string {
+  let h: string = "";
+  do {
+    h = Math.random().toString(16).substring(2, 15);
+  } while (_hashes[h]);
+  return h;
+}
+
+async function HTMLResponse(response: string): Promise<Response> {
+  return new Response(response, {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  });
+}
+
+async function JSONResponse(response: string | any): Promise<Response> {
+  if (typeof response !== 'string') response = JSON.stringify(response);
+  return new Response(response, {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
+
+export { hash, html, HTMLResponse, JSONResponse };
--- src/views/index.ts
+++ src/views/index.ts
@@ -8,10 +8,43 @@ export function Index(params: any) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+    <script>
+      function login() {
+        const form = document.getElementById('form_id');
+        
+        const body = {
+          username: form.elements.username.value,
+          password: form.elements.password.value
+        };
+        
+        return fetch(
+          '',
+          {
+            body: JSON.stringify({
+              username: form.elements.username.value,
+              password: form.elements.password.value
+            }),
+            method: 'POST'
+          }
+        )
+        .then(res => console.log(res))
+        .catch(e => console.error(e));
+      }
+    </script>
   </head>
   <body>
+    <form id="form_id" method="post">
+      <div>
+        <label for="username"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="username" required>
+
+        <label for="password"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+        
+        <button type="submit" onclick="login()">Login</button>
+      </div>
+    <form>
   </body>
 </html>
   `;
